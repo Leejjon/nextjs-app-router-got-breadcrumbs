e9c04c40e43da8d387059bff0fcd0b8c3c718ac5
Leejjon <leejjon@gmail.com>
2026-01-05 00:02:55 +0100

    Changing to use GoT api

---
+++ app/characters/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/dist/client/link";
+
+export default function Characters() {
+    return (
+        <div className="grid grid-cols-12 min-h-screen grid-rows-[auto_1fr]">
+            <div id="header" className="col-span-12 border-b-2 border-black">
+                <h1 className="text-6xl font-bold text-center m-2">My Game of Thrones character app</h1>
+            </div>
+            <div id="navbar" className="p-2 border-r-2 border-black col-span-2 flex flex-col gap-2">
+                <Link href="/">Characters</Link>
+            </div>
+            <div id="content" className="col-span-10 p-2">
+                <h2 className="text-2xl font-bold">Characters</h2>
+                <p>A list of all characters</p>
+            </div>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-import Link from "next/dist/client/link";
+import {redirect} from "next/navigation";
 
 export default function Home() {
-    return (
-        <div className="grid grid-cols-12 min-h-screen grid-rows-[auto_1fr]">
-            <div id="header" className="col-span-12 border-b-2 border-black">
-                <h1 className="text-6xl font-bold text-center m-2">My Pokémon Game</h1>
-            </div>
-            <div id="navbar" className="p-2 border-r-2 border-black col-span-2 flex flex-col gap-2">
-                <Link href="/">Wilderness</Link>
-                <Link href="/pokedex">Pokédex</Link>
-                <Link href="/team">Your Pokémon</Link>
-            </div>
-            <div id="content" className="col-span-10 p-2">
-                <h2 className="text-2xl font-bold">Wilderness</h2>
-                <p>A wild &lt;insert pokemon here&gt; appeared.</p>
-            </div>
-        </div>
-    );
+    redirect('/characters');
 }
